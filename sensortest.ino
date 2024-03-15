2edf69535bbba4617df78e0c80789f1d2b9286f5
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2024-03-15 10:04:50 +0900

    Update sensortest.ino

--- sensortest.ino
+++ sensortest.ino
@@ -7,7 +7,7 @@ void setup() {
 
 void loop() {
   
-  // 5v
+  //converting the 10-bit ADC (Analog-to-Digital Converter) reading (0-1023) to a voltage value (0-5V)
   float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
   int distance = 13*pow(volts, -1); // worked out from datasheet graph
   delay(1000); // slow down serial port 
