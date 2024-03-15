bfc8951098375e96b0faa207b26db7bee558c74b
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2024-03-15 09:56:03 +0900

    Create sensortest.ino

---
+++ sensortest.ino
@@ -0,0 +1,19 @@
+
+#define sensor A0 
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  
+  // 5v
+  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+  int distance = 13*pow(volts, -1); // worked out from datasheet graph
+  delay(1000); // slow down serial port 
+  
+  if (distance <= 30){
+    Serial.println(distance); 
+  }
+}
+//Sharp IR GP2Y0A41SK0F analog distance sensor 4-30cm
